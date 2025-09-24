aab8e4bac13f98698602ea9a2df96f2c7bd5cfa7
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-24 20:48:53 +0800

    feat: implemented user add func and navigation to customers

--- tests/main.robot
+++ tests/main.robot
@@ -7,4 +7,11 @@ TEST-000001
     [Documentation]     Verify first 5 users added details
     Launch Browser      url=${URL}
     Login Action        username=${USERNAME}    password=${PASSWORD}
+
+    ${users}            Get Random Users
+    FOR     ${user}     IN      @{users}
+        Navigate Customers
+        Create User     ${user}
+    END
+
     Sleep               3s
--- variables/users_variable.py
+++ variables/users_variable.py
@@ -5,3 +5,12 @@ PASSWORD = "demo"
 x_username_input = '//input[@name="username"]'
 x_password_input = '//input[@name="password"]'
 x_login_button = '//button[@type="submit"]'
+
+x_customers_link = '//a[@href="#/customers"]'
+x_create_customer_link = '//a[@href="#/customers/create"]'
+
+x_first_name_input = '//input[@name="first_name"]'
+x_last_name_input = '//input[@name="last_name"]'
+x_email_input = '//input[@name="email"]'
+x_save_button = '//button[@aria-label="Save"]'
+x_delete_button = '//button[@aria-label="Delete"]'
