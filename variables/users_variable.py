27be2218cf1f4c0f99abe2f859e98b91796df191
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-24 14:38:35 +0800

    feat: added users variables

---
+++ variables/users_variable.py
@@ -0,0 +1,7 @@
+URL = "https://marmelab.com/react-admin-demo/#/login"
+USERNAME = "demo"
+PASSWORD = "demo"
+
+x_username_input = '//input[@name="username"]'
+x_password_input = '//input[@name="password"]'
+x_login_button = '//button[@type="submit"]'
